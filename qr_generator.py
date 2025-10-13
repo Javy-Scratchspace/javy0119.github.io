431462544c9a1bec778933255ddd4a3caf5f10d1
Javy0119 <138019998+Javy0119@users.noreply.github.com>
2025-10-13 00:49:27 +0000

    Made Website QR Code

--- qr_generator.py
+++ qr_generator.py
@@ -1,14 +1,14 @@
 import qrcode
 
 # Data to encode
-data = "https://www.linkedin.com/in/javiercuevas"
+data = "https://javy-scratchspace.github.io/javy0119.github.io/"
 
 # Generate QR code
 qr = qrcode.QRCode(
     version=1,  # controls size of QR
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
-    border=4,
+    border=2,
 )
 qr.add_data(data)
 qr.make(fit=True)
@@ -17,4 +17,4 @@ qr.make(fit=True)
 img = qr.make_image(fill_color="black", back_color="white")
 
 # Save to a file
-img.save("linkedin_qr.png")
+img.save("JACC_website_qr.png")
